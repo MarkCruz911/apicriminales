649c04b517a9b929be2e432010de8526949bb504
Marco Cruz <marcoantoniocruzrojas@gmail.com>
2023-11-28 04:20:15 -0400

    nuevos cambios tres

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+const cors = require('cors');
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(cors());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
